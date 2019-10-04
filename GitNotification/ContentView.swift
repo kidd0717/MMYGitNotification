0b37df4b590c0eb19ce6049c297286557687a8fd
MMY <barry.ding@ichef.com.tw>
2019-10-04 15:08:59 +0800

    Initial Commit

---
+++ GitNotification/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitNotification
+//
+//  Created by PeiYao Ding on 2019/10/4.
+//  Copyright © 2019 MMY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
